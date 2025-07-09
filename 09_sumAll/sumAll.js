e2a6560259f1d742161ecf01e18ea776e49cfab2
Jiaqi Wang <yidaojin9@gmail.com>
2025-07-09 23:12:46 +0800

    Finish 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (!Number.isInteger(firstNumber)
+        || !Number.isInteger(secondNumber)
+        || firstNumber < 0
+        || secondNumber < 0) {
+            return 'ERROR';
+        }
+    
+    const startNum = Math.min(firstNumber, secondNumber);
+    const endNum = Math.max(firstNumber, secondNumber);
+    let sum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
