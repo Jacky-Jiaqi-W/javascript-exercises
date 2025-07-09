14c125e593c3573a4012b9e7f345309e3872182a
Jiaqi Wang <yidaojin9@gmail.com>
2025-07-09 22:49:31 +0800

    Finish 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    for (const item of arr) {
+        if (!args.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
