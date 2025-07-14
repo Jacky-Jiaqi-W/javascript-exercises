839e0aa199cc013b8ebd86bdf2ad6fbb4f938583
Jiaqi Wang <yidaojin9@gmail.com>
2025-07-14 23:41:14 +0800

    Finish 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort((person1, person2) => {
+        const person1Age = (person1.yearOfDeath === undefined)
+            ? (new Date()).getFullYear() - person1.yearOfBirth
+            : person1.yearOfDeath - person1.yearOfBirth
+        const person2Age = (person2.yearOfDeath === undefined)
+            ? (new Date()).getFullYear() - person2.yearOfBirth
+            : person2.yearOfDeath - person2.yearOfBirth
+        return person1Age - person2Age;
+    });
+    return array.at(-1);
 };
 
 // Do not edit below this line
