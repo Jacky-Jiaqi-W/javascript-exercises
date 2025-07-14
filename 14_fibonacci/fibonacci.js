c37372499162947477e39933b8515e36be33f719
Jiaqi Wang <yidaojin9@gmail.com>
2025-07-14 17:24:51 +0800

    Finish 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = +index;
+    if (index < 0) {
+        return "OOPS";
+    } else if (index < 2) {
+        return index;
+    }
+    let prevPrev = 0;
+    let prev = 1;
+    // const fibonacciSeq = [0, 1];
+    for (let i = 2; i <= index; i++) {
+        let curr = prevPrev + prev;
+        prevPrev = prev;
+        prev = curr;
+    }
+    return prev;
 };
 
 // Do not edit below this line
